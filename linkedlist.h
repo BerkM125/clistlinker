dbd4400e0a67bebe1bdbcc3191c4f5d580cf7152
BerkM125 <49427416+BerkM125@users.noreply.github.com>
2020-09-22 00:07:27 -0700

    Update linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -7,4 +7,4 @@ void PrintLinkedList(struct ListNode *p);
 void Prepend(struct ListNode *p, int value);
 void Append(struct ListNode *p, int value);
 void Remove(struct ListNode *p, int location);
-void Get(struct ListNode *p, int location);
+int Get(struct ListNode *p, int location);
